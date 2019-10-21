192064fed72f190f136d9dc56cd7a682c88f4c85
Bella Pratiwi <55772879+1914370120@users.noreply.github.com>
2019-10-21 02:22:54 -0700

    Add files via upload

---
+++ BELLA PRATIWI.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    printf("\tBIODATA PRIBADI\n\n");
+    printf("NAMA     : BELLA PRATIWI\n");
+    printf("NPM      : 1914370120\n");
+    printf("KELAS    : REGULER 1D\n");
+    printf("PRODI    : SISTEM KOMPUTER\n");
+    printf("FAKULTAS :SAINS & TEKNOLOGI\n");
+
+
+    int a=50;
+    int b=30;
+    int c;
+    c=a+b;
+    printf("baris 1 -nilai c adalah %d\n",c);
+    c=a-b;
+    printf("baris 2 -nilai c adalah %d\n",c);
+    c=a*b;
+    printf("baris 3 -nilai c adalah %d\n",c);
+    c=a/b;
+    printf("baris 4 -nilai c adalah %d\n",c);
+    c= b%a;
+    printf("baris 5 -nilai c adalah %d\n",c);
+    return 0;
+}
